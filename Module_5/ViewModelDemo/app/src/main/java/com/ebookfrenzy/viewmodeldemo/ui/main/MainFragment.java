09ec44adb452f8c0c5b08fb56bcaa04221ee6d3c
BLTinAnnArbor <tbl101@wccnet.edu>
2020-02-21 17:36:59 -0500

    update

--- Module_5/ViewModelDemo/app/src/main/java/com/ebookfrenzy/viewmodeldemo/ui/main/MainFragment.java
+++ Module_5/ViewModelDemo/app/src/main/java/com/ebookfrenzy/viewmodeldemo/ui/main/MainFragment.java
@@ -1,13 +1,13 @@
 package com.ebookfrenzy.viewmodeldemo.ui.main;
 
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,35 +16,71 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.ebookfrenzy.viewmodeldemo.R;
+import com.ebookfrenzy.viewmodeldemo.databinding.MainFragmentBinding;
 
+import static com.ebookfrenzy.viewmodeldemo.BR.myViewModel; // I had to add this manually !!
+                                                            // else it errored
 public class MainFragment extends Fragment {
 
-    private MainViewModel mViewModel;
-    private EditText dollarText;
-    private TextView resultText;
-    private Button convertButton;
-
-    public static MainFragment newInstance() {
+    // This is necessary
+       public static MainFragment newInstance() {
         return new MainFragment();
-    }
+     }
+
+
+
+    private MainViewModel mViewModel;
+
+    public MainFragmentBinding binding;
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+
+        binding = DataBindingUtil.inflate(
+                inflater, R.layout.main_fragment, container, false);
+
+        binding.setLifecycleOwner(this); // Ensures the binding instance is destroyed when the
+        // fragment goes away cuz the current fragment is the lifecycle owner of it.
+
+        return binding.getRoot(); // It is still returning the root view of the layout.
+
+        //return inflater.inflate(R.layout.main_fragment, container, false);
     }
+
+
+    //private EditText dollarText;
+    //private TextView resultText;
+    //private Button convertButton;
 
     @Override  // Called each time the Activity is created
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
         mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
 
+        // Assigning the ViewModel instance to the data binding variable.
+        binding.setVariable(myViewModel, mViewModel); //myViewModel is from XML
+/*
         dollarText = getView().findViewById(R.id.dollarText);
         resultText = getView().findViewById(R.id.resultText);
         convertButton = getView().findViewById(R.id.convertButton);
 
-        resultText.setText(mViewModel.getResult().toString());
+        // If the line below is commented out, the TextView info is lost after a rotation.
+        // Commented out because onChanged() will now be responsible for updating.
+        //resultText.setText(mViewModel.getResult().toString());
+
+        // Configure an observer
+        final Observer<Float> resultObserver = new Observer<Float>() {
+            @Override
+            public void onChanged(@Nullable final Float result) {
+                resultText.setText((result.toString()));
+            }
+        };
+
+        mViewModel.getResult().observe(this, resultObserver);
 
         // Add the listener
         convertButton.setOnClickListener(new View.OnClickListener()
@@ -53,9 +89,10 @@ public class MainFragment extends Fragment {
             public void onClick(View v){
 
                 if(!dollarText.getText().toString().equals("")){
-                    mViewModel.setAmount(dollarText.getText().toString()); // what if I
-                    resultText.setText(mViewModel.getResult().toString()); // switched these?
-                    //mViewModel.setAmount(dollarText.getText().toString()); // Then it lags behind by one
+
+                    mViewModel.setAmount(dollarText.getText().toString());
+                    //resultText.setText(mViewModel.getResult().toString());
+
                 }else {
                     resultText.setText("No Value was Entered.");
                 }
@@ -64,7 +101,7 @@ public class MainFragment extends Fragment {
 
         });
 
-
+ */
     }
 
 }
--- Module_5/ViewModelDemo/app/src/main/java/com/ebookfrenzy/viewmodeldemo/ui/main/MainViewModel.java
+++ Module_5/ViewModelDemo/app/src/main/java/com/ebookfrenzy/viewmodeldemo/ui/main/MainViewModel.java
@@ -1,21 +1,45 @@
 package com.ebookfrenzy.viewmodeldemo.ui.main;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
-    private static final Float usd_to_eu_rate = 0.74F;
-    private String dollarText = "";
-    private Float result = 0F;
 
+    private static final Float usd_to_eu_rate = 0.74F;
+
+    //private String dollarText = "";
+    public MutableLiveData<String> dollarValue = new MutableLiveData<>();
+
+    //private Float result = 0F;
+    //private
+    public MutableLiveData<Float> result = new MutableLiveData<>();
+
+    public void convertValue()
+    {
+        if ((dollarValue.getValue() != null) &&
+                (!dollarValue.getValue().equals("")))
+        {
+            result.setValue(Float.valueOf(dollarValue.getValue())
+            * usd_to_eu_rate);
+        }else
+            {
+            result.setValue(0.0F);
+        }
+    }
+/*
     public void setAmount(String value){
+
         this.dollarText = value;
-        result = Float.valueOf(dollarText)*usd_to_eu_rate;
+
+        //result = Float.valueOf(dollarText)*usd_to_eu_rate;
+        result.setValue(Float.valueOf(dollarText)*usd_to_eu_rate);
     }
 
-    public Float getResult()
+    //public Float getResult()
+    public MutableLiveData<Float> getResult()
     {
         return result;
     }
-
+ */
 
 }
