66e6e603f0e5653b38e754886b49ea3be74b239a
BLTinAnnArbor <tbl101@wccnet.edu>
2020-03-03 17:44:57 -0500

    update

---
+++ Module_6/TabLayoutDemo/app/src/main/java/com/ebookfrenzy/tablayoutdemo/TabPagerAdapter.java
@@ -0,0 +1,33 @@
+package com.ebookfrenzy.tablayoutdemo;
+
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabPagerAdapter extends FragmentPagerAdapter {
+
+    int tabCount;
+
+    public TabPagerAdapter(FragmentManager fm, int numberOfTabs){
+        super(fm);
+        this.tabCount = numberOfTabs;
+    }
+    @Override
+    public Fragment getItem(int position){
+        switch(position){
+        case 0:
+            return new FirstFragment();
+        case 1:
+            return new SecondFragment();
+        case 2:
+            return new ThirdFragment();
+        case 3:
+            return new FourthFragment();
+        default:
+            return null;
+        }
+    }
+    @Override
+    public int getCount(){
+        return tabCount;
+    }
+}
